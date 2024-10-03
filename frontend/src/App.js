0c6ef50f47d75a180b753d96c08de64e57adc8c3
Sanskar Chirania <sanskar125112@gmail.com>
2024-10-03 20:27:46 +0530

    Add Register component and route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/login";
 import ProtectedRoute from "./protectedRoute";
+import Register from "./components/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route
           path="/"
           element={<ProtectedRoute>{/* <AppContent /> */}</ProtectedRoute>}
